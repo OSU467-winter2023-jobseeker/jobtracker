8d14a69a0a1d0235066feca50305df734873bafb
Matt <mattlam08@gmail.com>
2023-02-04 21:36:25 -0500

    WIP ContactsAddModal

---
+++ client/src/components/ContactsAddModal.js
@@ -0,0 +1,56 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure
+} from '@chakra-ui/react'
+
+import {
+    Button,
+    React,
+    FormControl,
+    FormLabel,
+    Input,
+    Lorem,
+    Box
+} from 'react'
+
+function ContactsAddModal() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const finalRef = React.useRef(null)
+
+    return (
+        <>
+            <Box ref={finalRef} tabIndex={-1} aria-label='Focus moved to this box'>
+                Some other content that'll receive focus on close.
+            </Box>
+
+            <Button mt={4} onClick={onOpen}>
+                Open Modal
+            </Button>
+            <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Lorem count={2} />
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+};
+
+export default ContactsAddModal;
