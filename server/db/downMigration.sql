0c7da94112b7b42a398d44cea8ca9a924e081a0e
nervster <shethn@oregonstate.edu>
2023-02-09 23:57:14 -0600

    added database setup

---
+++ server/db/downMigration.sql
@@ -0,0 +1,5 @@
+DROP TABLE if exists skills CASCADE;
+DROP TABLE if exists contacts CASCADE;
+DROP TABLE  if exists contact_application CASCADE;
+DROP TABLE if exists users CASCADE;
+DROP TABLE  if exists applications CASCADE;
