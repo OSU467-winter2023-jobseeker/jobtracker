8006abe283c03e58277a8442c8216ca3c3a0df0f
Matthew Lam <lamma@engr.orst.edu>
2023-02-20 22:30:55 -0500

    Placeholder code for retrieving contacts

--- client/src/Pages/Contacts.js
+++ client/src/Pages/Contacts.js
@@ -1,8 +1,14 @@
 import { Box, Center, Container, Flex, VStack, Text, Heading, Divider, InputGroup } from '@chakra-ui/react';
 import ContactsTable from '../components/ContactsTable';
 import AddContactRow from '../components/AddContactRow';
+import React, {useEffect} from 'react';
 
-function Contacts() {
+function Contacts({loadContacts}) {
+    // Use effect to load the contacts data
+    useEffect(() => {
+        loadContacts();
+    }, []);
+
     return (
         <VStack>
             <Box>
