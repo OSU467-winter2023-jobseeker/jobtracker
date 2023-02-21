62198f77fe81ea0becf1b835f25bd755cb8f412e
Matthew Lam <lamma@engr.orst.edu>
2023-02-20 22:38:50 -0500

    Placeholder code for add contact

--- client/src/Pages/Contacts.js
+++ client/src/Pages/Contacts.js
@@ -1,14 +1,32 @@
-import { Box, Center, Container, Flex, VStack, Text, Heading, Divider, InputGroup } from '@chakra-ui/react';
+import { Box, Center, Container, Flex, VStack, Text, Heading, Divider, InputGroup, Button } from '@chakra-ui/react';
 import ContactsTable from '../components/ContactsTable';
 import AddContactRow from '../components/AddContactRow';
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Contacts({loadContacts}) {
+function Contacts({ loadContacts }) {
     // Use effect to load the contacts data
     useEffect(() => {
         loadContacts();
     }, []);
 
+    // Add useState
+    const [addContact, setAddContact] = useState({
+        company: "",
+        name: "",
+        position: "",
+        email: "",
+        phoneNumber: ""
+    });
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        const newAddContact = { ...addContact };
+        console.log(newAddContact);
+        // insert response with api
+
+        // clear data from addJobApplication
+    }
     return (
         <VStack>
             <Box>
@@ -21,9 +39,16 @@ function Contacts({loadContacts}) {
                 <Center>
                     <div>Add a new contact:</div>
                 </Center>
-                <AddContactRow />
+                <form onSubmit={handleFormSubmit}>
+                    <AddContactRow
+                        addContact={addContact}
+                        setAddContact={setAddContact}
+                    />
+                    <Button colorScheme='teal' variant='solid' marginLeft={2} type='submit'>
+                        Add
+                    </Button>
+                </form>
             </Box>
-
         </VStack>
 
     )
--- client/src/components/AddContactRow.js
+++ client/src/components/AddContactRow.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Center, Input, InputGroup } from '@chakra-ui/react';
 
 
-function AddContactRow() {
+function AddContactRow({addContact, setAddContact}) {
     // Pass in onChange handler and onSubmit
     return (
         <Center>
@@ -12,36 +12,43 @@ function AddContactRow() {
                     name="company"
                     required="required"
                     placeholder="Enter a company..."
+                    value={addContact.company}
+                    onChange={(e) => setAddContact({...addContact, company: e.target.value})}
                 />
                 <Input
                     type="text"
                     name="name"
                     required="required"
                     placeholder="Enter a name..."
+                    value={addContact.name}
+                    onChange={(e) => setAddContact({...addContact, name: e.target.value})}
                 />
                 <Input
                     type="text"
                     name="position"
                     required="required"
                     placeholder="Enter a position..."
+                    value={addContact.position}
+                    onChange={(e) => setAddContact({...addContact, position: e.target.value})}
                 />
                 <Input
                     type="email"
                     name="email"
                     required="required"
                     placeholder="Enter an email..."
+                    value={addContact.email}
+                    onChange={(e) => setAddContact({...addContact, email: e.target.value})}
                 />
                 <Input
                     type="text"
                     name="phoneNumber"
                     required="required"
                     placeholder="Enter a phone number..."
+                    value={addContact.phoneNumber}
+                    onChange={(e) => setAddContact({...addContact, phoneNumber: e.target.value})}
                 />
                 {/* Date we should autopopulate with the current date with an insert statement */}
             </InputGroup>
-            <Button colorScheme='teal' variant='solid' marginLeft={2}>
-                Add
-            </Button>
         </Center>
     )
 };
