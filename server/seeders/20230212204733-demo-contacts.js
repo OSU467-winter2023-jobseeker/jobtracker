6893ea4449f8aa8194aca08663db5a6cc2c94122
nervster <shethn@oregonstate.edu>
2023-02-21 21:44:20 -0600

    added more contact seed data

--- server/seeders/20230212204733-demo-contacts.js
+++ server/seeders/20230212204733-demo-contacts.js
@@ -12,7 +12,38 @@ module.exports = {
       linkedin_url: "http://linkedin.com/in/bob-googler",
       created_at: new Date(),
       updated_at: new Date()
-    }]);
+    },
+    {
+      user_id: 5,
+      full_name: "Bob Googler2",
+      position: 'GCP Manager2',
+      email: "varchar2@varchar.com",
+      phone_number: "555-675-8852",
+      linkedin_url: "http://linkedin.com/in/bob-googler2",
+      created_at: new Date(),
+      updated_at: new Date()
+    },
+    {
+      user_id: 5,
+      full_name: "Bob Googler3",
+      position: 'GCP Manager3',
+      email: "varchar3@varchar.com",
+      phone_number: "555-675-8853",
+      linkedin_url: "http://linkedin.com/in/bob-googler3",
+      created_at: new Date(),
+      updated_at: new Date()
+    },
+    {
+      user_id: 5,
+      full_name: "Bob Googler4",
+      position: 'GCP Manager4',
+      email: "varchar4@varchar.com",
+      phone_number: "555-675-8854",
+      linkedin_url: "http://linkedin.com/in/bob-googler4",
+      created_at: new Date(),
+      updated_at: new Date()
+    },
+  ]);
     
   },
 
