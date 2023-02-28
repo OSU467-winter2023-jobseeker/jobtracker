07345001035e92d1e3a3be59143767d7e3cde4ee
ellisos2 <ellisos2@oregonstate.edu>
2023-02-28 14:13:01 -0500

    Get from /skills now sends analytics data

--- server/controllers/skillsController.js
+++ server/controllers/skillsController.js
@@ -4,58 +4,81 @@ const pool = Pool.pool;
 
 async function getSkills (request, response) {
     const userId = await authorization.checkToken(request, true);
-    const applications = [];
+    var applications = [];
 
-    pool.query('SELECT a.application_id FROM applications a INNER JOIN users u ON u.user_id = a.user_id WHERE a.user_id = $1 ORDER BY a.created_at DESC',
-        [request.params.id],
+    pool.query('SELECT s.skill, a.employer FROM skills s INNER JOIN (SELECT a.application_id, a.employer FROM applications a WHERE a.user_id = $1) a ON a.application_id = s.application_id ORDER BY s.created_at DESC',
+        [userId],
         (error, results) => {
             if (error) {
                 throw error
             }
-            console.log(results.rows)
-            return;
+            const skillsResults = analyzeSkills(results.rows);
+            console.log(skillsResults);
+            response.status(200).json(skillsResults);
     });
 
-    pool.query('SELECT s.* FROM skills s INNER JOIN applications a ON a.application_id = s.application_id WHERE s.application_id = $1 ORDER BY s.created_at DESC', 
-        [request.params.id], 
-        (error, results) => {
-            if (error) {
-                throw error
-            }
-            response.status(200).json(results.rows);
-        }
-    );
 };
+
+function analyzeSkills (skillsQuery) {
+    var skillsResults = {}
+    var apps = [];
+    
+    for (const skillPair in skillsQuery) {
+        const currentSkill = skillsQuery[skillPair].skill;
+        const currentEmployer = skillsQuery[skillPair].employer;
+        if (!(currentSkill in skillsResults)) {
+            skillsResults[currentSkill] = {skillPercent: 1, applications: [currentEmployer]};
+        } else {
+            skillsResults[currentSkill].skillPercent += 1;
+            skillsResults[currentSkill].applications.push(currentEmployer);
+        }
+
+        if (!apps.includes(currentEmployer)) {apps.push(currentEmployer)};
+    }
+
+    const appCount = apps.length;
+
+    for (const skill in skillsResults) {
+        const skillCount = skillsResults[skill].skillPercent;
+        skillsResults[skill].skillPercent = (100 * skillCount / appCount).toFixed(0); 
+    }
+
+    return skillsResults;
+}
 
 const getSkill = (request, response) => {
     pool.query('SELECT s.* FROM skills s INNER JOIN applications a ON a.application_id = s.application_id WHERE s.application_id = $1 ORDER BY s.created_at DESC', 
         [request.params.id], 
         (error, results) => {
             if (error) {
-                throw error
+                throw error;
             }
             response.status(200).json(results.rows);
         }
     );
 };
 
+/**
+ * Take the string of skills inserted into applications and 
+ * create a new Skill object for each.
+ */
 async function createSkills (applicationId, skills) {
     const skillList = skills.split(', ');
     for (const skill in skillList) {
-        const newSkill = await createSkill(applicationId, skill);
-        console.log(newSkill);
+        const newSkill = await createSkill(applicationId, skillList[skill]);
     }
 }
 
+/**
+ * Create a single new skill entity.
+ */
 async function createSkill (applicationId, skill) {
-    
     pool.query('INSERT INTO skills (application_id, skill, comfort_level) VALUES ($1, $2, $3)',
         [applicationId, skill, '5'],
         (error, results) => {
             if (error) {
                 throw error;
             }
-            console.log(results);
             return results;
         }
     );
